38177191e9b9dbca8946bdde2cd1be034b4e9210
Ajeet Gill <ajeetshergill@gmail.com>
2023-11-04 20:25:09 -0300

    solution-TCPserver.py

--- TCPServer.py
+++ TCPServer.py
@@ -1,12 +1,53 @@
 from socket import *
-serverPort = 13000
+serverPort = 8000
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(('',serverPort))
 serverSocket.listen(1)
 print ('The server is ready to receive')
-# while True:
-connectionSocket, addr = serverSocket.accept()
-sentence = connectionSocket.recv(1024).decode()
-capitalizedSentence = sentence.upper()
-connectionSocket.send(capitalizedSentence.encode())
-connectionSocket.close()
+print ('localhost:8000')
+request_count = 0
+
+try:
+     while True:
+          #  accept the incoming HTTP get request from the client and decode it
+          connectionSocket, addr = serverSocket.accept()
+          request = connectionSocket.recv(1024).decode('utf-8')
+          #  parse the request to determine the specific file being requested from the client
+          request_lines = request.split('\n')
+          http_request_line = request_lines[0].split()
+          method, relative_filename, version = http_request_line
+          file_to_serve = relative_filename[1:]
+
+          response = ""
+          try:
+               if(file_to_serve == ""): # if no file is specified, serve index.html
+                    # file_to_serve == "" 
+                    # it's a common practive that if there's no file specified, the server will serve the index.html file so I added this rule
+                    file_to_serve = "index.html"
+
+               with open(file_to_serve, 'rb') as file: 
+                    data = file.read()  # Read the file content
+                    request_count += 1
+                    print(request_count,"Serving file:",file_to_serve)
+                    response = "HTTP/1.1 200 OK\r\n\r\n".encode('utf-8') + data
+
+          except FileNotFoundError:
+               print("'"+file_to_serve+"'- File not found.")
+               response = "HTTP/1.1 404 Not Found\r\n\r\n404 Not Found : File not found.".encode('utf-8')
+
+          except Exception as e:
+               print(f"An error occurred: {e}")
+               response = "HTTP/1.1 404 Not Found\r\n\r\n404 Not Found : Unknown error occurred.".encode('utf-8')
+
+          finally:
+               if 'file' in locals():
+                    file.close()  # Close the file if it's open
+          
+          connectionSocket.sendall(response)
+          connectionSocket.close()
+
+except KeyboardInterrupt:
+     print(" - Server terminated by user.")
+
+finally:
+    serverSocket.close()
